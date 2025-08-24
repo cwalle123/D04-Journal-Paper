18ecfdd153b99763723f1e8502d32011552b5ca5
cwalle123 <wcliftonjohn@gmail.com>
2025-08-24 16:59:21 +0200

    Create Model_ALL_Validation-Tow_Visualiser.py

---
+++ Scripts/Model_ALL_Validation-Tow_Visualiser.py
@@ -0,0 +1,28 @@
+"""This file is used to generate a figure of a simulated tow vs an experimental tow"""
+
+"""This file is currently not being used for anything except plotting"""
+
+##############################################################################################################
+
+# External imports
+import matplotlib.pyplot as plt
+import numpy as np
+
+#Internal imports
+from Handling_ALL_Functions import get_synced_data
+from constants import Consecutive_Error_Bins
+
+##############################################################################################################
+"""Functions"""
+
+# TODO add a function that gets a tow from traverse experimental data team and simulated data team and plots the in the same graph for comparison
+
+
+##############################################################################################################
+"""Run this file"""
+
+def main():
+    tow = 3
+
+if __name__ == "__main__":
+    main() # makes sure this only runs if you run *this* file, not if this file is imported somewhere else
