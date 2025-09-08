2fe4201c75192d50f63df8a60a8a379898d1f328
MartijnVDV1 <m.l.a.vandervoort@student.tudelft.nl>
2025-09-08 16:32:05 +0200

    Intermediate save

--- Scripts/Handling_ALL_Functions.py
+++ Scripts/Handling_ALL_Functions.py
@@ -223,16 +223,20 @@ def get_synced_data(tow: int, sensor_type: str, overwrite=False, helper=False) -
 
 def traverse_vs_layup_data(tow: int):
 
-    path_traverse = r'C:\Users\manue\OneDrive\Documents\GitHub\D04-Journal-Paper\Synced data from Siddharth\ExportedCSVs\Traverse\Traverse tracker data\TrackerData_7_Traverse.csv'
-    path_layup = r'C:\Users\manue\OneDrive\Documents\GitHub\D04-Journal-Paper\Synced data from Siddharth\ExportedCSVs\Layup data\Data\Data_Run07_Tracker.csv'
+    #path_traverse = r'C:\Users\manue\OneDrive\Documents\GitHub\D04-Journal-Paper\Synced data from Siddharth\ExportedCSVs\Traverse\Traverse tracker data\TrackerData_7_Traverse.csv'
+    #path_layup = r'C:\Users\manue\OneDrive\Documents\GitHub\D04-Journal-Paper\Synced data from Siddharth\ExportedCSVs\Layup data\Data\Data_Run07_Tracker.csv'
+
+    path_traverse = f"Cached Data/TRAVERSE_GAP_{tow}.csv"
+    path_layup = f"Cached Data/LAYUP_{tow}.csv"
 
     traverse = pd.read_csv(path_traverse)
     layup = pd.read_csv(path_layup)
 
-    plt.plot(traverse.iloc[:, 2], traverse.iloc[:, 3], label="Traverse", linestyle='-',linewidth=1.5, color='b')
+    # Plot traverse dataframe
+    plt.plot((traverse.iloc[:, 2] * 500 + 500 * traverse.iloc[:, 0]), (0.5 * traverse.iloc[:, 3] + 0.5 * traverse.iloc[:, 1]), label="Traverse", linestyle='-',linewidth=1.5, color='b')
 
     # Plot layup dataframe
-    plt.plot(layup.iloc[:, 2], layup.iloc[:, 3], label="Layup", linestyle='-',linewidth=1.5, color='r')
+    plt.plot(layup.iloc[:, 7], (layup.iloc[:, 8] + layup.iloc[:, 3]), label="Layup", linestyle='-',linewidth=1.5, color='r')
 
     # Labels and title
     plt.xlabel("x")
@@ -251,16 +255,16 @@ def traverse_vs_layup_data(tow: int):
 def main():
 
     # Just to check if the new data with weights is correct (it is)
-    for tow in range(1,32):
-        x = get_synced_data(tow, "TRAVERSE_GAP")
-        print(np.shape(x))
-    print("Columns:", x.columns.tolist())
+    #for tow in range(1,32):
+    #    x = get_synced_data(tow, "TRAVERSE_GAP")
+    #    print(np.shape(x))
+    #print("Columns:", x.columns.tolist())
     #print()
     #print(get_synced_data(5, "TRAVERSE"))
     #print()
     #print(get_synced_data(5, "LT"))
 
-    # traverse_vs_layup_data(20)
+    traverse_vs_layup_data(10)
     
 if __name__ == "__main__":
     main() # makes sure this only runs if you run *this* file, not if this file is imported somewhere else
