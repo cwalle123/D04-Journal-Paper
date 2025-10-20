727628b57232d3b54db8ccb78c66a83daebaba69
seanmc <seanmcsean11@gmail.com>
2025-10-20 13:26:54 +0100

    starting variation plots now functioning

--- Scripts/Model_ALL_StartVariations.py
+++ Scripts/Model_ALL_StartVariations.py
@@ -36,7 +36,7 @@ def calc_lengthwise_defect_percent(tows: int, num_divisions: int=31, starting_mo
         total_layout_area = np.trapezoid(highest_tow_edge[int(start):int(end)] - lowest_tow_edge[int(start):int(end)], x_vals)
         total_gap_area = sum(np.trapezoid(np.clip(values, 0, None), x_vals) for values in data.values.T)
         total_overlap_area = sum(np.trapezoid(np.clip(-values, 0, None), x_vals) for values in data.values.T)
-        print(total_layout_area, total_gap_area, total_overlap_area)
+        print(total_layout_area, total_gap_area, total_overlap_area, len(x_vals), len(lowest_tow_edge[int(start):int(end)]))
 
         gap_percent = (total_gap_area / total_layout_area) * 100 if total_layout_area > 0 else 0
         overlap_percent = (total_overlap_area / total_layout_area) * 100 if total_layout_area > 0 else 0
@@ -56,8 +56,25 @@ def calc_lengthwise_defect_percent(tows: int, num_divisions: int=31, starting_mo
     print(defect_data)
     return defect_data
 
-def plot_lengthwise_defect_percent(defect_data: pd.DataFrame):
-    plt.plot(defect_data["x"], defect_data["defect_percent"], label="Defect Percentage")
+def plot_lengthwise_defect_percent(defect_data_original: pd.DataFrame, defect_data_modified: pd.DataFrame):
+    # original data
+    #plt.scatter(defect_data_original["x"], defect_data_original["defect_percent"], color="blue", marker='o')
+    #plt.plot(defect_data_original["x"], defect_data_original["defect_percent"], color="blue", linestyle='solid', label="Defect Percentage, normal")
+    plt.scatter(defect_data_original["x"], defect_data_original["gap_percent"], color="green", marker='o')
+    plt.plot(defect_data_original["x"], defect_data_original["gap_percent"], color="green", linestyle='solid', label="Gap Percentage, normal")
+    plt.scatter(defect_data_original["x"], defect_data_original["overlap_percent"], color="red", marker='o')
+    plt.plot(defect_data_original["x"], defect_data_original["overlap_percent"], color="red", linestyle='solid', label="Overlap Percentage, normal")
+    # modified data
+    #plt.scatter(defect_data_modified["x"], defect_data_modified["defect_percent"], color="blue", marker='^')
+    #plt.plot(defect_data_modified["x"], defect_data_modified["defect_percent"], color="blue", linestyle='dashed', label="Defect Percentage, modified")
+    plt.scatter(defect_data_modified["x"], defect_data_modified["gap_percent"], color="green", marker='^')
+    plt.plot(defect_data_modified["x"], defect_data_modified["gap_percent"], color="green", linestyle='dashed', label="Gap Percentage, modified")
+    plt.scatter(defect_data_modified["x"], defect_data_modified["overlap_percent"], color="red", marker='^')
+    plt.plot(defect_data_modified["x"], defect_data_modified["overlap_percent"], color="red", linestyle='dashed', label="Overlap Percentage, modified")
+
+    plt.xlabel("x (mm)")
+    plt.ylabel("Defect Percentage (%)")
+    plt.legend()
     plt.show()
 
 
@@ -68,6 +85,7 @@ def analyze_starting_variation_effects(starting_mods: list=[None, 1, 1], proposa
     LT_steps, LT_proposal_std, LT_target_dist, LT_dist, LT_params = fit_random_walk("LT")
     CAM_steps, CAM_proposal_std, CAM_target_dist, CAM_dist, CAM_params = fit_random_walk("CAM")
     LLS_B_steps, LLS_B_proposal_std, LLS_B_target_dist, LLS_B_dist, LLS_B_params = fit_random_walk("LLS_B")
+    print(f"distributions type per error:\n LT:{LT_dist}\n CAM:{CAM_dist}\nLLS_B:{LLS_B_dist}")
 
     # This seciton modifies the starting distributions used by the model, which is needed for Model_ALL_StartVariations.
     if starting_mods != [None, 1, 1]:
@@ -98,10 +116,11 @@ def analyze_starting_variation_effects(starting_mods: list=[None, 1, 1], proposa
 
 
 def main():
-    analyze_starting_variation_effects(starting_mods = [None, 1, 2], proposal_type = "RWM")
+    #analyze_starting_variation_effects(starting_mods = [None, 1, 2], proposal_type = "RWM")
 
-    #defect_data = calc_lengthwise_defect_percent(100, starting_mods=[None, 1, 1.5])
-    #plot_lengthwise_defect_percent(defect_data)
+    defect_data_original = calc_lengthwise_defect_percent(500, num_divisions=31, starting_mods=[None, 1, 1])
+    defect_data_modified = calc_lengthwise_defect_percent(500, num_divisions=31, starting_mods=[None, 1, 1.5])
+    plot_lengthwise_defect_percent(defect_data_original, defect_data_modified)
 
 if __name__ == "__main__":
     main()
