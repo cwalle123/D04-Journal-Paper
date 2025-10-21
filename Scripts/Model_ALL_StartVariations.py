97b444883613084f0e5d740f81f02558b5d0ac8d
seanmc <seanmcsean11@gmail.com>
2025-10-21 22:51:20 +0100

    made plots of target vs. starting distributions for CAM. Made function to collect data for multiple laminates of a certain number of tows.

--- Scripts/Model_ALL_StartVariations.py
+++ Scripts/Model_ALL_StartVariations.py
@@ -9,14 +9,30 @@ import seaborn as sns
 # Internal imports
 from constants import tow_width_specified, font_extra_small, font_small, font_medium, font_large, font_extra_large
 from Handling_ALL_Functions import get_synced_data
-from Model_ALL_ConsecutiveErrorTheo import consecutive_error, generate_error_path, generate_starting_error
 from Data_ALL_statistics import main as real_hist, plot_histograms_separated, best_fit_distribution
 from Model_ALL_RandomWalk import fit_random_walk, generate_random_walk, generate_RW_multitow
-from Model_ALL_Simulation import generate_multitow_layout
-from Model_ALL_RandomSampling import generate_RS_multitow
+from Model_ALL_Simulation import fit_starting_error_distribution
+from scipy.stats import norm, logistic, gamma, beta, expon, lognorm, skewnorm, gumbel_r, gumbel_l, genextreme
 
 
 
+def generate_multilaminate_layout(n_laminates: int, tows_per_laminate: int=31, starting_mods: list=[None, 1, 1]):
+    # generating and combining data for all laminates
+    gap_overlap_dict = {}
+    all_RW_data = []
+    for laminate in range(n_laminates):
+        temp_gap_overlap_df, gap_df, overlap_df, total_gap_percent, total_overlap_percent, RW_data = generate_RW_multitow(
+            num_tows=tows_per_laminate, starting_mods=starting_mods)
+        temp_gap_overlap_df = np.array(temp_gap_overlap_df)
+
+        temp_gap_overlap_dict = {
+            f"Gap/overlap_Laminate{laminate}_Tow{tow + 1}-{tow + 2}": temp_gap_overlap_df[:, tow]
+            for tow in range(tows_per_laminate - 1)}
+        gap_overlap_dict.update(temp_gap_overlap_dict)
+        all_RW_data.append(RW_data)
+
+    gap_overlap_df = pd.DataFrame(gap_overlap_dict)
+    return gap_overlap_df, all_RW_data          # TODO: find a way to calc (defect) areas for multiple laminates... );
 
 def calc_lengthwise_defect_percent(tows: int, num_divisions: int=31, starting_mods: list=[None, 1, 1]):
     gap_overlap_df, gap_df, overlap_df, total_gap_percent, total_overlap_percent, RW_data = generate_RW_multitow(num_tows=tows, starting_mods=starting_mods)
@@ -115,12 +131,61 @@ def analyze_starting_variation_effects(starting_mods: list=[None, 1, 1], proposa
                                           LLS_B_params, proposal_type=proposal_type, plot_path=True, plot_histogram=True)
 
 
+def plot_target_vs_start(target_mods: list=[None, 1, 1], starting_mods: list=[1, 1]):
+    x_pdf = np.linspace(-1.2, 1.2, 100)     # this is for the plotting later on.
+
+    ### Code for the target distributions ###
+    CAM_steps, CAM_proposal_std, CAM_target_dist, CAM_dist, CAM_params = fit_random_walk("CAM")
+    y_target = CAM_target_dist(x_pdf)
+
+    # This section modifies the starting distributions used by the model, which is needed for Model_ALL_StartVariations.
+    if target_mods != [None, 1, 1]:
+        if target_mods[0] != None:  # this changes the starting distribution type if necessary
+            CAM_dist_modded = target_mods[0]
+        else: CAM_dist_modded = CAM_dist
+        # these are the factors by which the mean and std are changed
+        loc_factor, scale_factor = target_mods[1], target_mods[2]
+        # code used for start value: start_value = dist.rvs(*params[:-2], loc=params[-2], scale=params[-1])
+        CAM_params_modded = list(CAM_params)
+        CAM_params_modded[-2] *= loc_factor
+        CAM_params_modded[-1] *= scale_factor  # TODO: make sure the scale parameters equally affect the different distribution types
+        CAM_params_modded = tuple(CAM_params_modded)
+        CAM_target_dist_modded = lambda x: CAM_dist_modded.pdf(x, *CAM_params_modded[:-2], loc=CAM_params_modded[-2], scale=CAM_params_modded[-1])
+        y_target_modded = CAM_target_dist_modded(x_pdf)
+        plt.plot(x_pdf, y_target_modded, label='modified target distribution')
+
+    ### Code for the starting value distributions ###
+    mean, std, _, _, first_values = fit_starting_error_distribution("CAM", plot=False)
+    CAM_starting_dist = lambda x: norm.pdf(x, loc=mean, scale=std)
+    y_starting = CAM_starting_dist(x_pdf)
+
+    # This section modifies the starting distributions used by the model, which is needed for Model_ALL_StartVariations.
+    if starting_mods != [1, 1]:
+        # these are the factors by which the mean and std are changed
+        loc_factor, scale_factor = starting_mods[0], starting_mods[1]
+        mean_modded = mean * loc_factor
+        std_modded = std * scale_factor
+        CAM_starting_dist_modded = lambda x: norm.pdf(x, loc=mean_modded, scale=std_modded)
+        y_starting_modded = CAM_starting_dist_modded(x_pdf)
+        plt.plot(x_pdf, y_starting_modded, label='modified target distribution')
+
+
+    plt.hist(first_values, bins=10, density=True, alpha=0.3, label='experimental starting values')
+    plt.plot(x_pdf, y_target, label='target distribution')
+    plt.plot(x_pdf, y_starting, label='starting values distribution')
+    plt.title("starting and target distributions")
+    plt.xlim(-1.2, 1.2)
+    plt.legend()
+    plt.show()
+
+
 def main():
     #analyze_starting_variation_effects(starting_mods = [None, 1, 2], proposal_type = "RWM")
+    #plot_target_vs_start()      # target_mods=[None, 1, 1.5], starting_mods=[1, 1.5]
 
-    defect_data_original = calc_lengthwise_defect_percent(500, num_divisions=31, starting_mods=[None, 1, 1])
-    defect_data_modified = calc_lengthwise_defect_percent(500, num_divisions=31, starting_mods=[None, 1, 1.5])
-    plot_lengthwise_defect_percent(defect_data_original, defect_data_modified)
+    defect_data_original = calc_lengthwise_defect_percent(3, tows_per_laminate=5, num_divisions=31, starting_mods=[None, 1, 1])
+    #defect_data_modified = calc_lengthwise_defect_percent(3, tows_per_laminate=5, num_divisions=31, starting_mods=[None, 1, 1.5])
+    #plot_lengthwise_defect_percent(defect_data_original, defect_data_modified)
 
 if __name__ == "__main__":
     main()
