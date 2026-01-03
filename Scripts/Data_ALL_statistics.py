fixed the distribution fitting function to correctly fit again, removed weighting from the RW proposal distribution fitting.

Scripts/Data_ALL_statistics.py
@@ -249,7 +249,7 @@ def plot_LLSA_vs_LLSB(data: pd.DataFrame, title:str, bin_widths: list[float] =No
         plt.show()
 
 # Functions for fitting and collecting data:
-def best_fit_distribution(data, bins=40, distributions=None, weights=None, use_all_dist=False, plot=False, print_statement=False, shrink_scale_factor=1.0): 
+def best_fit_distribution(data, bins=40, distributions=None, weights=None, use_all_dist=False, plot=False, print_statement=False):
     '''This function fits the best probability distribution to the four error types automatically, for the weighted data''' 
     # Compute the histogram of the data 
     y, bin_edges = np.histogram(data, bins=bins, density=True, weights=weights) 
@@ -260,65 +260,66 @@ def best_fit_distribution(data, bins=40, distributions=None, weights=None, use_a
     
     # If no distribution list given, use a broad default set 
     if distributions is None and use_all_dist is True: 
-        distributions = [skewnorm] 
+        distributions = [skewnorm]
     elif distributions is None and use_all_dist is False: 
-        distributions = [norm, logistic] 
+        distributions = [logistic, norm]
     
     best = {'dist': None, 'params': None, 'mse': np.inf} 
-    # Iterate over each candidate distribution 
-    for dist in distributions: 
+    # Iterate over each candidate distribution
+    for dist in distributions:
         # Skip distributions that require non-negative data if data has negatives 
         #if data.min() < 0 and dist in (gamma, beta, expon, lognorm, skewnorm, gumbel_r, genextreme): 
         # continue 
-        with warnings.catch_warnings(): 
-            warnings.filterwarnings('ignore') 
-            try: 
-                # Fit the best distribution to the data 
+        with warnings.catch_warnings():
+            warnings.filterwarnings('ignore')
+            try:
+                # Fit the best distribution to the data
                 params = dist.fit(data)
-                params = list(params) 
-                params[-1] = params[-1] * shrink_scale_factor 
-                params = tuple(params)
-                
-                # Evaluate its PDF at the bin centers 
-                pdf = dist.pdf(x_mid, *params[:-2], loc=params[-2], scale=params[-1]) 
-                # Compute sum of squared errors between histogram and PDF to check accuracy 
-                mse = sklearn.mean_squared_error(y, pdf) 
-                # If this fit is better (lower MSE), use it 
-                if mse < best['mse']: 
-                    best.update(dist=dist, params=params, mse=mse) 
-            except Exception: 
-                # If fitting fails for any reason, skip to the next distribution 
-                continue 
+                # params = list(params)         # These 3 lines are old code to improve CAM fit which we are NOT using
+                # params[-1] = params[-1] * 1
+                # params = tuple(params)
+
+                # Evaluate its PDF at the bin centers
+                pdf = dist.pdf(x_mid, *params[:-2], loc=params[-2], scale=params[-1])
+                # Compute sum of squared errors between histogram and PDF to check accuracy
+                mse = sklearn.mean_squared_error(y, pdf)
+                # If this fit is better (lower MSE), use it
+                if mse < best['mse']:
+                    best.update(dist=dist, params=params, mse=mse)
+
+            except Exception:
+                # If fitting fails for any reason, skip to the next distribution
+                continue
         
-        best_dist = best['dist'] 
-        params = best['params'] 
-        mse = best['mse'] 
-        
-        # shape parameters (can be empty) 
-        shapes = params[:-2] 
-        loc = params[-2] 
-        scale = params[-1] 
-        
-        params = (*shapes, loc, scale) 
-        
-        if print_statement == True: 
-            print("Best:", best_dist.name) 
-            print("Shape parameters:", shapes) 
-            print("loc:", loc, "scale:", scale) 
-            print("MSE:", mse) 
-        
-        # Return the distribution with the lowest error (MSE) 
-        
-        if plot: 
-            x = np.linspace(best_dist.ppf(0.001, *shapes, loc=loc, scale=scale), best_dist.ppf(0.999, *shapes, loc=loc, scale=scale), 200) 
-            plt.hist(data, bins=bins, density=True, alpha=0.5) 
-            plt.plot(x, best_dist.pdf(x, *shapes, loc=loc, scale=scale), linewidth=2) 
-            plt.title(f"Best fit: {best_dist.name}") 
-            plt.xlabel("Value") 
-            plt.ylabel("Density") 
-            plt.show() 
-        
-        return best
+    best_dist = best['dist']
+    params = best['params']
+    mse = best['mse']
+
+    # shape parameters (can be empty)
+    shapes = params[:-2]
+    loc = params[-2]
+    scale = params[-1]
+
+    params = (*shapes, loc, scale)
+
+    if print_statement == True:
+        print("Best:", best_dist.name)
+        print("Shape parameters:", shapes)
+        print("loc:", loc, "scale:", scale)
+        print("MSE:", mse)
+
+    # Return the distribution with the lowest error (MSE)
+
+    if plot:
+        x = np.linspace(best_dist.ppf(0.001, *shapes, loc=loc, scale=scale), best_dist.ppf(0.999, *shapes, loc=loc, scale=scale), 200)
+        plt.hist(data, bins=bins, density=True, alpha=0.5)
+        plt.plot(x, best_dist.pdf(x, *shapes, loc=loc, scale=scale), linewidth=2)
+        plt.title(f"Best fit: {best_dist.name}")
+        plt.xlabel("Value")
+        plt.ylabel("Density")
+        plt.show()
+
+    return best
 
 def build_all_sensors_df(tow_range=range(2, 31), time_key=None):
     """Returns a dataframe with columns:
@@ -474,7 +475,7 @@ def main():
         bins = 100
     data, weights = np.array(get_data(sensor, format="merged"))
     print(f"Sensor: {sensor}")
-    best = best_fit_distribution(data=data, bins=bins, distributions=None, weights=weights, use_all_dist=use_all_dist, plot=True, print_statement=True, shrink_scale_factor=shrink_scale_factor)
+    best = best_fit_distribution(data=data, bins=bins, distributions=None, weights=weights, use_all_dist=use_all_dist, plot=True, print_statement=True)
     print(best)
 
 if __name__ == "__main__":

Scripts/Model_ALL_ConsecutiveModeler.py
@@ -31,7 +31,7 @@ from Model_ALL_RandomSampling import generate_RS_multitow
 ##############################################################################################################
 """Functions"""
 
-def plot_RW_vs_exp_histograms(RW_tows: int=100, save_PDF: bool=True):
+def plot_RW_vs_exp_histograms(RW_tows: int=100, save_PDF: bool=True, use_RW_fit: bool=False):
     """This function creates the plot of Random Walk vs Experimental Data for each individual sensor.
     This is equivalent to plot 2 in the paper atm."""
 
@@ -47,6 +47,11 @@ def plot_RW_vs_exp_histograms(RW_tows: int=100, save_PDF: bool=True):
     LLS_A_steps, LLS_A_proposal_std, LLS_A_target_dist, LLS_A_dist, LLS_A_params = fit_random_walk("LLS_A", bins=100)
     LLS_B_steps, LLS_B_proposal_std, LLS_B_target_dist, LLS_B_dist, LLS_B_params = fit_random_walk("LLS_B", bins=100)
 
+    print(f"LT distribution = {LT_dist} \n"
+          f"CAM distribution = {CAM_dist} \n"
+          f"LLS_A distribution = {LLS_A_dist} \n"
+          f"LLS_B distribution = {LLS_B_dist}")
+
     LT_walk_data, CAM_walk_data, LLSA_walk_data, LLSB_walk_data = [], [], [], []
     for tow in range(RW_tows):
         LT_walk_data += generate_random_walk("LT", LT_steps, LT_proposal_std, LT_target_dist, LT_dist, LT_params,
@@ -57,7 +62,6 @@ def plot_RW_vs_exp_histograms(RW_tows: int=100, save_PDF: bool=True):
                                               LLS_A_params, proposal_type="RWM")
         LLSB_walk_data += generate_random_walk("LLS_B", LLS_B_steps, LLS_B_proposal_std, LLS_B_target_dist, LLS_B_dist,
                                               LLS_B_params, proposal_type="RWM")
-    print(LT_exp, len(LT_exp))
 
 
     distribution_label_names = {
@@ -80,47 +84,54 @@ def plot_RW_vs_exp_histograms(RW_tows: int=100, save_PDF: bool=True):
     x_pdf_LLS_A = np.linspace(-0.6, 0, 75)
     x_pdf_LLS_B = np.linspace(-0.3, 0.3, 75)
 
-    #LT
-    best_LT = best_fit_distribution(LT_exp, bins=100)
-    best_LT_dist = best_LT['dist']
-    params_LT = best_LT['params']
-    mse_LT = best_LT['mse']
-    shapes_LT = params_LT[:-2]    
-    loc_LT = params_LT[-2]
-    scale_LT = params_LT[-1]
-    y_pdf_LT = best_LT_dist.pdf(x_pdf_LT, *shapes_LT, loc=loc_LT, scale=scale_LT)
+    if use_RW_fit:
+        y_pdf_LT = LT_target_dist(x_pdf_LT)
+        y_pdf_CAM = CAM_target_dist(x_pdf_CAM)
+        y_pdf_LLS_A = LLS_A_target_dist(x_pdf_LLS_A)
+        y_pdf_LLS_B = LLS_B_target_dist(x_pdf_LLS_B)
 
-    #CAM
-    shrink_scale_factor_CAM = 0.9 # This factor is used to artifically stretch the skewnorm distribution to visually better fit the histogram
-    best_CAM = best_fit_distribution(CAM_exp, bins=250, use_all_dist=True, shrink_scale_factor=shrink_scale_factor_CAM)
-    best_CAM_dist = best_CAM['dist']
-    params_CAM = best_CAM['params']
-    mse_CAM = best_CAM['mse']
-    shapes_CAM = params_CAM[:-2] 
-    loc_CAM = params_CAM[-2]
-    scale_CAM = params_CAM[-1]
-    print(f'Scale_CAM: {scale_CAM}')
-    y_pdf_CAM = best_CAM_dist.pdf(x_pdf_CAM, *shapes_CAM, loc=loc_CAM, scale=scale_CAM)
+    else:
+        #LT
+        best_LT = best_fit_distribution(LT_exp, bins=100)
+        best_LT_dist = best_LT['dist']
+        params_LT = best_LT['params']
+        mse_LT = best_LT['mse']
+        shapes_LT = params_LT[:-2]
+        loc_LT = params_LT[-2]
+        scale_LT = params_LT[-1]
+        y_pdf_LT = best_LT_dist.pdf(x_pdf_LT, *shapes_LT, loc=loc_LT, scale=scale_LT)
 
-    #LSS A
-    best_LLSA = best_fit_distribution(LLSA_exp, bins=100)
-    best_LLSA_dist = best_LLSA['dist']
-    params_LLSA = best_LLSA['params']
-    mse_LLSA = best_LLSA['mse']
-    shapes_LLSA = params_LLSA[:-2]    
-    loc_LLSA = params_LLSA[-2]
-    scale_LLSA = params_LLSA[-1]
-    y_pdf_LLS_A = best_LLSA_dist.pdf(x_pdf_LLS_A, *shapes_LLSA, loc=loc_LLSA, scale=scale_LLSA)
+        #CAM
+        #DON'T USE: shrink_scale_factor_CAM =  0.9 # This factor is used to artifically stretch the skewnorm distribution to visually better fit the histogram
+        best_CAM = best_fit_distribution(CAM_exp, bins=250, use_all_dist=True)
+        best_CAM_dist = best_CAM['dist']
+        params_CAM = best_CAM['params']
+        mse_CAM = best_CAM['mse']
+        shapes_CAM = params_CAM[:-2]
+        loc_CAM = params_CAM[-2]
+        scale_CAM = params_CAM[-1]
+        print(f'Scale_CAM: {scale_CAM}')
+        y_pdf_CAM = best_CAM_dist.pdf(x_pdf_CAM, *shapes_CAM, loc=loc_CAM, scale=scale_CAM)
 
-    #LLS B
-    best_LLSB = best_fit_distribution(LLSB_exp, bins=100)
-    best_LLSB_dist = best_LLSB['dist']
-    params_LLSB = best_LLSB['params']
-    mse_LLSB = best_LLSB['mse']
-    shapes_LLSB = params_LLSB[:-2]    
-    loc_LLSB = params_LLSB[-2]
-    scale_LLSB = params_LLSB[-1]
-    y_pdf_LLS_B = best_LLSB_dist.pdf(x_pdf_LLS_B, *shapes_LLSB, loc=loc_LLSB, scale=scale_LLSB)
+        #LSS A
+        best_LLSA = best_fit_distribution(LLSA_exp, bins=100)
+        best_LLSA_dist = best_LLSA['dist']
+        params_LLSA = best_LLSA['params']
+        mse_LLSA = best_LLSA['mse']
+        shapes_LLSA = params_LLSA[:-2]
+        loc_LLSA = params_LLSA[-2]
+        scale_LLSA = params_LLSA[-1]
+        y_pdf_LLS_A = best_LLSA_dist.pdf(x_pdf_LLS_A, *shapes_LLSA, loc=loc_LLSA, scale=scale_LLSA)
+
+        #LLS B
+        best_LLSB = best_fit_distribution(LLSB_exp, bins=100)
+        best_LLSB_dist = best_LLSB['dist']
+        params_LLSB = best_LLSB['params']
+        mse_LLSB = best_LLSB['mse']
+        shapes_LLSB = params_LLSB[:-2]
+        loc_LLSB = params_LLSB[-2]
+        scale_LLSB = params_LLSB[-1]
+        y_pdf_LLS_B = best_LLSB_dist.pdf(x_pdf_LLS_B, *shapes_LLSB, loc=loc_LLSB, scale=scale_LLSB)
 
     #Start of plotting
     mpl.rcParams['font.family'] = 'serif'
@@ -1152,7 +1163,7 @@ def main():
     #Gap_Histogram(30)
     #KDE_curves(29)
     #model_distribution_figures(29, plottype="single no D04", save_PDF=True)
-    plot_RW_vs_exp_histograms(RW_tows=31, save_PDF=False)
+    plot_RW_vs_exp_histograms(RW_tows=31, save_PDF=True, use_RW_fit=False)
 
 if __name__ == "__main__":
     main() # makes sure this only runs if you run *this* file, not if this file is imported somewhere else

Scripts/Model_ALL_RandomWalk.py
@@ -54,8 +54,9 @@ def fit_random_walk(sensor: str, bins=40):
     if sensor != "CAM":
         best = best_fit_distribution(np.array(data), bins=bins, weights=np.array(weights))
     elif sensor == "CAM":
-        best = best_fit_distribution(np.array(data), weights=np.array(weights), use_all_dist=True, shrink_scale_factor=0.9)
+        best = best_fit_distribution(np.array(data), weights=np.array(weights), use_all_dist=True)
     dist, params = best['dist'], best['params']
+    print(f"for {sensor}, the dist is: {dist}")
     target_distribution = lambda x: dist.pdf(x, *params[:-2], loc=params[-2], scale=params[-1])
 
     proposal_std = get_proposal_distribution(sensor)
@@ -291,8 +292,13 @@ def get_proposal_distribution(sensor, plot: bool=False):
         weights.append(weight)
 
     # determining the normal distribution which fits:
-    mean = np.average(data, weights= weights)
-    variance = np.average((data-mean)**2, weights=weights)
+    mean = np.average(data)
+    variance = np.var(data)
+    ### old code for weighted distributions against Bias ###
+    #weighted_mean = np.average(data, weights= weights)
+    #weighted_variance = np.average((data-mean)**2, weights=weights)
+    #print(f"Mean: {mean}, Variance: {variance} \n"
+    #      f"weighted: Mean: {weighted_mean}, Variance: {weighted_variance}")
     std = np.sqrt(variance)
 
     if plot:
@@ -1228,9 +1234,9 @@ def generate_virtual_lamina_figure(num_tows=5, tow_spacing_mm=6.35, tow_width_mm
 """Run this file"""
 
 def main():
-    # LT_steps, LT_proposal_std, LT_target_dist, LT_dist, LT_params = fit_random_walk("CAM")
-    #LT_walk_data = generate_random_walk("LT", LT_steps, LT_proposal_std, LT_target_dist, LT_dist, LT_params,
-    #                            proposal_type="RWM", plot_histogram=True, plot_path=True)
+    LT_steps, LT_proposal_std, LT_target_dist, LT_dist, LT_params = fit_random_walk("LT")
+    LT_walk_data = generate_random_walk("LT", LT_steps, LT_proposal_std, LT_target_dist, LT_dist, LT_params,
+                                proposal_type="RWM", plot_histogram=True, plot_path=True)
     #plot_RW_tows(proposal_type="RWM", plot_individual_histograms=True)
     #std = get_proposal_distribution("CAM", plot=True)
     #plot_animated_walk_hist("CAM", 100)
@@ -1250,8 +1256,8 @@ def main():
 
     # generate_random_walk(sensor='CAM', n_steps=LT_steps, proposal_std=LT_proposal_std, target_dist=LT_target_dist, dist=LT_dist, params=LT_params, proposal_type='RWM', plot_histogram=True, return_pdf=True)
     #test_advanced_RW()
-    analyze_tow_spacing_effect(existing_data='Cached Data/Tow_spacing_effect_RWM_with_100_simulations_of_a_29_tow_laminate.csv',
-                              error_areas=True, error_bars=False, save_PDF=True)
+    #analyze_tow_spacing_effect(existing_data='Cached Data/Tow_spacing_effect_RWM_with_100_simulations_of_a_29_tow_laminate.csv',
+    #                          error_areas=True, error_bars=False, save_PDF=True)
     #test_LLS_A_B_condition()
     #generate_virtual_lamina_figure(save_PDF=True)
     #find_RW_statistics(n_tows=1000)